f8c39e03267baec51594da93f12ea7b915804ac6
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-08-12 03:31:02 +0000

    developed the front end of the about section further

--- src/js/component/aboutUs.js
+++ src/js/component/aboutUs.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import "../../styles/aboutUs.css";
 
 const AboutUs = () => {
@@ -86,6 +88,85 @@ const AboutUs = () => {
               </p>
             </div>
           </div>
+          <h5 className="pb-2">My Learning</h5>
+          <div className="timeline-item">
+            <div className="timeline-content">
+              <h5 className="timeline-title">Fullstack Development</h5>
+              <h6 className="timeline-date">
+                <a
+                  href="https://4geeksacademy.com/us/index?utm_source=google&utm_medium=cpc&utm_campaign=11031489831&utm_content=107548070545&utm_term=4geeks%20academy&cq_src=google_ads&cq_cmp=11031489831&cq_con=107548070545&cq_term=4geeks%20academy&cq_med=&cq_plac=&cq_net=g&cq_plt=gp&gad_source=1&gclid=Cj0KCQjwwuG1BhCnARIsAFWBUC2mF2bO-1k8irX7OcJBxXar7KQKoWBnoaD3r9RVkGE8qgkqDvR3usgaAsgpEALw_wcB"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  4Geeks
+                </a>
+              </h6>
+              <p className="timeline-description">
+                At 4Geeks Academy Miami, I immersed myself in a comprehensive
+                Fullstack Development program that shaped my journey from a
+                novice to a proficient developer. This rigorous curriculum
+                blended cutting-edge technologies with hands-on projects,
+                empowering me to master both front-end and back-end development.
+                Through real-world coding challenges and collaborative teamwork,
+                I honed my skills in React, Python, and more, building robust
+                applications from the ground up. This transformative experience
+                not only deepened my technical expertise but also ignited my
+                passion for crafting innovative, scalable solutions in the
+                ever-evolving world of technology.
+              </p>
+            </div>
+          </div>
+          <div className="timeline-item">
+            <div className="timeline-content">
+              <h5 className="timeline-title">React - The Complete Guide</h5>
+              <h6 className="timeline-date">
+                <a
+                  href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Udemy
+                </a>
+              </h6>
+              <p className="timeline-description">
+                This immersive program provided a thorough grounding in React,
+                covering everything from the basics to advanced techniques,
+                including state management with Redux. Through engaging lessons
+                and hands-on projects, I mastered the skills needed to build
+                dynamic, high-performance web applications. This course not only
+                deepened my understanding of React but also equipped me with
+                practical tools to create scalable and efficient solutions in
+                the ever-evolving tech landscape.
+              </p>
+            </div>
+          </div>
+          <div className="timeline-item">
+            <div className="timeline-content">
+              <h5 className="timeline-title">Data Structures and Algorithms</h5>
+              <h6 className="timeline-date">
+                <a
+                  href="https://www.udemy.com/course/master-the-coding-interview-data-structures-algorithms/?couponCode=KEEPLEARNING"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Udemy
+                </a>
+              </h6>
+              <p className="timeline-description">
+                This course provided a rigorous and detailed exploration of
+                essential data structures and algorithms, from fundamental
+                concepts to advanced problem-solving techniques. Through
+                hands-on exercises and real-world coding challenges, I developed
+                a robust understanding of how to efficiently tackle complex
+                problems and optimize performance. This course enhanced my
+                ability to design and implement scalable, high-quality software
+                solutions.
+              </p>
+            </div>
+          </div>
+          <p>
+            Let's continue to projects. <FontAwesomeIcon icon={faArrowRight} />
+          </p>
         </div>
       </div>
       <footer className="footer-text">
